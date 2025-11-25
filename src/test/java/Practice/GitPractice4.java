74c2588cade49d2a44bdbd15fd0bf6245bf1be4d
DELL <DELL@DESKTOP-DP80P86>
2025-11-25 21:43:10 +0530

    second branch push

---
+++ src/test/java/Practice/GitPractice4.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class GitPractice4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pushing through branch");
+		
+	}
+
+}
