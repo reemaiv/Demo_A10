8b64ab16d846871b977564ecdb4916eb8c23fc25
DELL <DELL@DESKTOP-DP80P86>
2025-11-26 16:59:49 +0530

    push from framework

--- src/test/java/Practice/GitPractice3.java
+++ src/test/java/Practice/GitPractice3.java
@@ -5,6 +5,7 @@ public class GitPractice3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Good Morning");
+		System.out.println("pushing through frame work engineer");
 	}
 
 }
